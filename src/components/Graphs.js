1c4192cfb1e2962fa1911bb0588e34b4529f8027
amyhagelin <ahagelin@gmail.com>
2017-08-04 09:57:54 -0700

    graph styling

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -58,24 +58,36 @@ class Graphs extends Component {
                 scales: {
                     xAxes: [{
                         type: 'time',
+                        gridLines: {
+                            zeroLineColor: 'transparent'
+                        },
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            drawTicks: false,
+                            display: false
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1
+                        }
                     }]
                 }
             },
-            // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
                 {
                 label: 'Seizure Frequency',
                 // fill: false,
                 lineTension: 0.1,
-                backgroundColor: 'rgba(75,192,192,0.4)',
-                borderColor: 'rgba(75,192,192,1)',
+                backgroundColor: 'transparent',
+                borderColor: 'transparent',
                 borderCapStyle: 'butt',
                 borderDash: [],
                 borderDashOffset: 0.0,
                 borderJoinStyle: 'miter',
                 pointBorderColor: 'rgba(75,192,192,1)',
                 pointBackgroundColor: '#fff',
-                pointBorderWidth: 1,
+                pointBorderWidth: 4,
                 pointHoverRadius: 5,
                 pointHoverBackgroundColor: 'rgba(75,192,192,1)',
                 pointHoverBorderColor: 'rgba(220,220,220,1)',
